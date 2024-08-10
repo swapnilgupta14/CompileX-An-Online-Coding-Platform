d8ec8243ba1fedbbad7a8565860196a93b6c6161
swapnilgupta14 <swapnilguptaswag@gmail.com>
2024-08-10 15:56:56 +0530

    MyApp change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,17 @@
 import { useEffect } from 'react';
+import Sidebar from './Sidebar';
 import '../styles/global.scss';
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+      <div className="app-container">
+          <Sidebar index={1}/>
+          <div className="main-content">
+              <Component {...pageProps} />
+          </div>
+      </div>
+  );
 }
 
 export default MyApp;
