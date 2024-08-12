b97766a24757a54d249d3d18ec4742a039db802d
swapnilgupta14 <swapnilguptaswag@gmail.com>
2024-08-13 03:25:19 +0530

    layout change

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,32 @@
-import Layout from '../pages/Layout';
 import '../styles/global.scss';
+import Sidebar from './Sidebar';
+import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  const titleMap = {
+    '/Home': 'Home - Compilex',
+    '/Editor': 'Playground  - Compilex',
+    '/Battleground/Battles': 'Battleground  - Compilex',
+    '/Arena/[id]': 'Arena - Compilex',
+  };
+
+  const title = titleMap[router.pathname] || 'Compilex';
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>{title}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="app-container"></div>
+      <Sidebar index={1} />
+      <div className="main-content">
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
 
