d8a7a4926cd7328e57f55cd61d1b2c76fe559211
swapnilgupta14 <swapnilguptaswag@gmail.com>
2024-08-14 04:14:08 +0530

    added preconnect - google fonts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,17 @@
-import { Html, Head, Main, NextScript } from "next/document";
+// _document.js
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head/>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100..700;1,100..700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
