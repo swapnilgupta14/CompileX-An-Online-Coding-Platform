0c9e5a4abc339f8b1dc867673553ca71d46c7d5a
Swapnil Gupta <85231522+swapnilgupta14@users.noreply.github.com>
2024-08-16 23:34:53 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -21,8 +21,8 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="header">
-        <h1 className="header-title">Problem</h1>
-        <p className="header-subtitle">Solve challenges, enhance your skills</p>
+        <h1 className="header-title">Compilex</h1>
+        <p className="header-subtitle">Solve challenges, practice, battle and enhance your skills</p>
       </div>
       <div className='home-content'>
         <HomeSection title="Problems" />
